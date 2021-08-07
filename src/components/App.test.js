2245595255a53908b7c76e632930d75d42532a7b
Luis Guilherme F <luis-guilherme.pereira@unesp.br>
2021-08-07 14:12:13 -0300

    tests and coverage added

---
+++ src/components/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+test('App snapshot test', () => {
+  const component = renderer.create(<App />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
